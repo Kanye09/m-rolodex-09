84a7568e5660e965bb4cd6f7182e0a0a394c7f5e
Kanybek Almanbet <kanyalmanbet@gmail.com>
2022-10-03 18:34:56 -0500

    sub component converted from class to func

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,23 +1,16 @@
-import React, { Component } from "react";
 import "./search-box.style.css";
 
-class SearchBox extends Component {
-  render() {
-    const { onChangeHandler } = this.props;
-    const { placeholder } = this.props;
-    const { className } = this.props;
-
-    return (
-      <div>
-        <input
-          type="search"
-          className={className}
-          placeholder={placeholder}
-          onChange={onChangeHandler}
-        />
-      </div>
-    );
-  }
-}
+const SearchBox = ({ onChangeHandler, placeholder, className }) => {
+  return (
+    <div>
+      <input
+        type="search"
+        className={className}
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
